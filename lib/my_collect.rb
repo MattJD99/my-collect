6d61504b0f52ea21c089a1ca84c0da1345fc6866
Matt D <dayclawtel@gmail.com>
2021-02-28 12:17:11 -0800

    commit tree f2fc872306374899b9c5d9049e83a2b542a6f7ff

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+require 'pry'
+def my_collect(array)
+    i = 0
+    result = []
+    while i < array.length
+      result.push(yield array[i])
+        i += 1
+    end
+    result
+  end
+  
+  array = ["ruby", "javascript", "python", "objective-c"]
+my_collect(array) do |lang|
+    lang.upcase
+end
 
-
+  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+  my_collect(array) do |name|
+    name.split(" ").first
+  end
